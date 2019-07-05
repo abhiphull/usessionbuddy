37378f806e02c1de3f726fae25c673bcf77b4bba
root <root@ns303271.ip-94-23-41.eu>
2019-07-05 03:14:24 +0200

    initial commit

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ import sys
 import os
 import subprocess
 
-if len(sys.argv) != 2:
-    print("Provide Usession buddy username")
+if len(sys.argv) != 3:
+    print("Provide Usession buddy username as ist argument\n")
+    print("Provide your server ip address as 2nd argument")
     sys.exit()
 username = sys.argv[1]
+ip_address = sys.argv[2]
 if sys.version_info[0] < 3:
     raise Exception("Must be using Python 3")
 
@@ -25,5 +27,6 @@ if os.path.isfile('conda.sh'):
 os.system('pip install TermRecord')
 os.system('echo "#!/bin/sh" > conda.sh')
 os.system("echo username=%s >> conda.sh"%username)
+os.system("echo ip_address=%s >> conda.sh"%ip_address)
 os.system("cat conda.sh.bak >> conda.sh")
 os.system('cp -p conda.sh /etc/profile.d/')
