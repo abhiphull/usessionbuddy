e93238db9148011a84529dbb117a7d7589cac4a1
root <root@ns303271.ip-94-23-41.eu>
2019-07-05 02:28:02 +0200

    initial commit

---
+++ conda.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+username=arpanphull
+
+if [ ! -d ~/sessions ]; then
+    mkdir -p ~/sessions
+fi
+
+if [ "x$SESSION_RECORD" = "x" ]
+then
+    timestamp=`date "+%m%d%Y%H%M"`
+    output=~/sessions/session.$USER.$$.$timestamp
+    SESSION_RECORD=started
+    export SESSION_RECORD
+    ########################
+    uniqueid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    histf=$HISTFILE 
+    usession_hist_f=/tmp/usession.history
+    if [ -f $usession_hist_f ];then
+        mv -f $usession_hist_f ${usession_hist_f}_${uniqueid}.prev
+    fi
+    export HISTFILE=/tmp/usession.history
+    ########################
+
+    ########################
+    if [ -f /home/sessions.info ];then
+        f=$(tail -1 /home/sessions.info)
+        if [ -f "$f" ]; then
+            mv -f $f ${f}_${uniqueid}.prev 
+        fi
+        sf=${f}_${uniqueid}.prev
+        hf=${usession_hist_f}_${uniqueid}.prev
+        if [ -f $hf ] && [ -f $sf ];then
+            echo "Running following command background \n"
+            echo "curl --header 'Content-Type: multipart/form-data' -XPOST -F hf=@$hf -F sf=@$sf -F \"username=${username}\"  http://localhost:5000"
+            curl --header 'Content-Type: multipart/form-data' -XPOST -F hf=@$hf -F sf=@$sf -F "username=${username}"  http://localhost:5000 &
+            echo "\n"
+        fi
+    fi
+    ########################
+
+    TermRecord -o ${output}.html
+    echo "${output}.html" >> /home/sessions.info
+    exit
+fi
