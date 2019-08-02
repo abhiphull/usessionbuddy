85e8351c47f9f26e06f9a8a3a69a89241c98fcaf
root <root@ns303271.ip-94-23-41.eu>
2019-08-02 12:18:10 +0200

    modified upload_session_buddy.py

--- youupload/upload_session_buddy.py
+++ youupload/upload_session_buddy.py
@@ -33,17 +33,17 @@ CLI.add_argument(
   type=str,
   required=True,
 )
+
+CLI.add_argument(
+  "--dont_upload_sessionfile", 
+  type=str,
+)
 args = CLI.parse_args()
 upload_script_file = args.upload_script_file
 username = args.username
 ip_address = args.ip_address
 recorder_type = args.recorder_type
-
-def ubuddy(hf,sf,username,ip_address,script_file=""):
-    if script_file:
-        os.system("curl --header 'Content-Type: multipart/form-data' -XPOST -F hf=@%s -F sf=@%s -F username=%s -F ip_address=%s -F script_file=@%s http://api.usessionbuddy.com"%(hf,sf,username,ip_address,script_file))
-    else:
-        os.system("curl --header 'Content-Type: multipart/form-data' -XPOST -F hf=@%s -F sf=@%s -F username=%s -F ip_address=%s http://api.usessionbuddy.com"%(hf,sf,username,ip_address))
+dont_upload_sessionfile = args.dont_upload_sessionfile
 
 
 
@@ -63,14 +63,19 @@ if __name__ == '__main__':
             rid = line
             if not hf:
                 hf = '/tmp/usession.%s.history'%rid
-            if not sf:
-                sf = '/tmp/usession.%s.html'%rid
+            if not dont_upload_sessionfile or dont_upload_sessionfile is None:
+                if not sf:
+                    sf = '/tmp/usession.%s.html'%rid
+            else:
+                sf = None
             #scriptfile
             script_file = '/tmp/usession.%s.log'%rid
             #print(hf,sf,username,ip_address,script_file)
-            if upload_script_file:
-                ubuddy(hf,sf,username,ip_address,script_file)
+            if upload_script_file and sf and hf:
+                os.system("curl --header 'Content-Type: multipart/form-data' -XPOST -F hf=@%s -F sf=@%s -F username=%s -F ip_address=%s -F script_file=@%s http://api.usessionbuddy.com"%(hf,sf,username,ip_address,script_file))
+            elif hf and sf:
+                os.system("curl --header 'Content-Type: multipart/form-data' -XPOST -F hf=@%s -F sf=@%s -F username=%s -F ip_address=%s http://api.usessionbuddy.com"%(hf,sf,username,ip_address))
             else:
-                ubuddy(hf,sf,username,ip_address)
+                os.system("curl --header 'Content-Type: multipart/form-data' -XPOST -F hf=@%s -F username=%s -F ip_address=%s http://api.usessionbuddy.com"%(hf,username,ip_address))
             break
                 
